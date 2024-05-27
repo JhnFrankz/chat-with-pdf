a6aae2f0e1620e2e56d1033419f40ee34400c427
Jhan Frank <jhnfrankz@gmail.com>
2024-05-27 12:11:14 -0500

    Add API endpoint for file upload

---
+++ src/pages/api/upload.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  const formData = await request.formData();
+  const file = formData.get("file");
+
+  console.log(file);
+
+  //simulate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return new Response("Hello, world!");
+};
