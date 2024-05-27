a852708c65b225f6714266d5a83bdeb11761a796
Jhan Frank <jhnfrankz@gmail.com>
2024-05-26 22:00:58 -0500

    Add conditional rendering for different app statuses

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,6 +1,25 @@
 <script>
   import { Alert } from "flowbite-svelte";
+  import { APP_STATUS, appStatus } from "../store.ts";
 </script>
+
+{#if $appStatus === APP_STATUS.INIT}
+  <p>La página inicial</p>
+{:else if $appStatus === APP_STATUS.LOADING}
+  <p>Loading...</p>
+{:else if $appStatus === APP_STATUS.ERROR}
+  <Alert>
+    <span class="font-medium">¡Algo malo ha pasado!</span>
+    Hay un error en la aplicación.
+  </Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+  <p>Chat Mode</p>
+{:else}
+  <Alert>
+    <span class="font-medium">Acción no reconocida</span>
+    Este estado de la aplicación no está reconocido
+  </Alert>
+{/if}
 
 <Alert>
   <span class="font-medium">Default alert!</span>
